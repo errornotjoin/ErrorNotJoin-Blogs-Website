bd09afd2e7fcbd86f56f62c85ac2f71fd4a0e16b
ErrorNotJoin <86501620+errornotjoin@users.noreply.github.com>
2023-04-05 23:30:17 +0100

    Small update to account.html

--- Errornotjoinforms.com/js/Creating_The_account.js
+++ Errornotjoinforms.com/js/Creating_The_account.js
@@ -5,3 +5,25 @@ Makeing_The_account.addEventListener("click", function Add_the_new_info()
     CreatIng_div.style.display = "Block"
     What_i_made.style.display = "none"
 })
+var Submit_for_user_ceating_account = document.getElementById("Submit_for_user_ceating_account")
+Submit_for_user_ceating_account.addEventListener("click", function adding_the_new_info()
+{
+    var How_name_user_info = 4;
+    for(var x = 1; x <= 4; x++)
+    {
+       var User_new_info = document.getElementById("User_INFO_" + x )
+       var User_old_info = document.getElementById("old_User_info_" + x)
+       if(x == 4 || User_new_info.value != "<script>")
+       {
+        User_old_info.innerHTML = User_new_info.value
+       }
+       else if(User_new_info.value == "<script>")
+       {
+        console.log("Error");
+       }
+       else
+       {
+        User_old_info.textContent = User_new_info.value
+       }
+    }
+})
