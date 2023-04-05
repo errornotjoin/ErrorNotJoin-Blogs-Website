1682c14e62c90c324527a3acdbda14f2bd92f51c
ErrorNotJoin <86501620+errornotjoin@users.noreply.github.com>
2023-04-05 14:16:53 +0100

    Update
    Update to Account.html
    New Items:
                      User_made_account.css
                      Creating_The_Account.js
    Added To Account.html

---
+++ Errornotjoinforms.com/js/Creating_The_account.js
@@ -0,0 +1,7 @@
+var Makeing_The_account = document.getElementById("Makeing_The_account")
+Makeing_The_account.addEventListener("click", function Add_the_new_info()
+{
+    var CreatIng_div = document.getElementById("CreatIng_div")
+    CreatIng_div.style.display = "Block"
+    What_i_made.style.display = "none"
+})
