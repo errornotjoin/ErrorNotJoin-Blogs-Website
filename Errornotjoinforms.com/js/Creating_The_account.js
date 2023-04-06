94a67386b7c9585ad527e8c51558c6c4023449d3
ErrorNotJoin <86501620+errornotjoin@users.noreply.github.com>
2023-04-06 15:37:17 +0100

    Update

--- Errornotjoinforms.com/js/Creating_The_account.js
+++ Errornotjoinforms.com/js/Creating_The_account.js
@@ -7,29 +7,24 @@ Makeing_The_account.addEventListener("click", function Add_the_new_info()
     CreatIng_div.style.display = "Block"
     What_i_made.style.display = "none"
 })
+var date = ["","", "User Age:", "Account was made on:"]
 var Submit_for_user_ceating_account = document.getElementById("Submit_for_user_ceating_account")
 Submit_for_user_ceating_account.addEventListener("click", function adding_the_new_info()
 {
     var How_name_user_info = 4;
-    for(var x = 1; x <= 4; x++)
+    for(var x = 1; x <= How_name_user_info; x++)
     {
-       var User_new_info = document.getElementById("User_INFO_" + x )
-       var User_old_info = document.getElementById("old_User_info_" + x)
-       if(x == 4 || User_new_info.value != "<script>")
-       {
-        User_old_info.innerHTML = User_new_info.value
-       
-        CreatIng_div.style.display = "none"
-        What_i_made.style.display = "none"
-
-       }
-       else if(User_new_info.value == "<script>")
-       {
-        console.log("Error");
-       }
-       else
-       {
-        User_old_info.textContent = User_new_info.value
-       }
+        var User_new_info = document.getElementById("User_INFO_" + x )
+        var User_old_info = document.getElementById("old_User_info_" + x)
+        if(x == 4)
+        {
+            User_old_info.innerHTML = User_new_info.value
+            CreatIng_div.style.display = "none"
+            What_i_made.style.display = "block"
+        }
+        else
+        {
+            User_old_info.textContent = date[x].toString() +" "+  User_new_info.value
+        }
     }
 })
